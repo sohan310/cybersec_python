e79d462d6f874e1311b8f4df09a50fe137a14ce3
sohan310 <sohan0310grd@gmail.com>
2022-12-22 17:59:57 +0530

    22dec

---
+++ pro1/prac.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+print ("Tuple is ({},{})".format(a, b))
---
+++ pro1/tesla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+x = int(input("Enter num:"))
+y = int(input("Enter num:"))
+print ("Values before swaping:",x,y)
+x = x + y
+y = x - y
+x = x - y
+print ("Values after swaping:",x,y)
