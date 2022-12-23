200927952da7c1a33340d1202fd6df6e2209955d
sohan310 <sohan0310grd@gmail.com>
2022-12-23 18:22:57 +0530

    dec23

--- pro1/script.py
+++ pro1/script.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-import argparse
+import argparse, os
 parser = argparse.ArgumentParser(description="This is test tool")
 parser.add_argument("-d",type=str, help="Type domain", required=True)
 
-parser.add_argument("-c",type=str, help="Type domain", required=True)
 a=parser.parse_args()
-print(a)
-print(a.d)
-print(a.c)
+os.system("host -t A {}".format(a.d))
 
 
